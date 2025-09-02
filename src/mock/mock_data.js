2717bf5736be868d35ed333c16ad9909cd1939fa
iceboooom <hzwzslasl@163.com>
2025-09-03 00:05:45 +0800

    chore: 更新导航数据 - 2025/9/3 00:05:42

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -54,6 +54,20 @@ export const mockData = {
           "url": "https://bbs.nga.cn/read.php?tid=29897651",
           "description": "smartwsw",
           "icon": ""
+        },
+        {
+          "id": "site-1756828675615",
+          "name": "捕鱼人小型攻略（部分过期）",
+          "url": "https://bbs.nga.cn/read.php?tid=21013520",
+          "description": "meww丶@沃仙曦染",
+          "icon": ""
+        },
+        {
+          "id": "site-1756828685961",
+          "name": "新人渔夫手册（部分过期）",
+          "url": "https://bbs.nga.cn/read.php?tid=20810560",
+          "description": "七生烟火@摩杜纳",
+          "icon": ""
         }
       ]
     },
@@ -62,34 +76,260 @@ export const mockData = {
       "icon": "🎮",
       "name": "钓场之王",
       "order": 1,
-      "sites": []
+      "sites": [
+        {
+          "id": "site-1756828699508",
+          "name": "鱼类休眠期（CD频率）",
+          "url": "https://ff14.huijiwiki.com/wiki/%E4%BC%91%E7%9C%A0%E6%9C%9F",
+          "description": "",
+          "icon": ""
+        },
+        {
+          "id": "site-1756828713369",
+          "name": "全鱼王鱼皇攻略",
+          "url": "https://bbs.nga.cn/read.php?tid=42310798",
+          "description": "光之渔夫",
+          "icon": ""
+        },
+        {
+          "id": "site-1756828726116",
+          "name": "7.x捕鱼攻略",
+          "url": "https://bbs.nga.cn/read.php?tid=42415384",
+          "description": "轩辕十四@沃仙曦染",
+          "icon": ""
+        },
+        {
+          "id": "site-1756828757406",
+          "name": "5.0鱼王鱼皇攻略",
+          "url": "https://bbs.nga.cn/read.php?tid=28794500",
+          "description": "颜君行@琥珀原、Aki秋山@摩杜纳",
+          "icon": ""
+        },
+        {
+          "id": "site-1756828736399",
+          "name": "6.0版本鱼王攻略",
+          "url": "https://bbs.nga.cn/read.php?tid=31412435",
+          "description": "轩辕十四@沃仙曦染",
+          "icon": ""
+        },
+        {
+          "id": "site-1756828745824",
+          "name": "6.x和3.x鱼类咬钩率",
+          "url": "https://bbs.nga.cn/read.php?tid=40146919",
+          "description": "轩辕十四@沃仙曦染",
+          "icon": ""
+        },
+        {
+          "id": "site-1756828813970",
+          "name": "3.0和4.0鱼皇攻略",
+          "url": "https://bbs.nga.cn/read.php?tid=18484723",
+          "description": "月咏泠音@摩杜纳",
+          "icon": ""
+        },
+        {
+          "id": "site-1756828825044",
+          "name": "5.5鱼王+5.55鱼皇攻略",
+          "url": "https://bbs.nga.cn/read.php?tid=27667271",
+          "description": "久藤彩鸟@沃仙曦染",
+          "icon": ""
+        },
+        {
+          "id": "site-1756828837659",
+          "name": "烟波204鱼王鱼皇攻略",
+          "url": "https://ngabbs.com/read.php?tid=23672580",
+          "description": "谈清若风柔@神拳痕",
+          "icon": ""
+        },
+        {
+          "id": "site-1756828847878",
+          "name": "鱼王鱼饵对应表格",
+          "url": "https://bbs.nga.cn/read.php?tid=20486405",
+          "description": "香辣脆皮喵@萌芽池",
+          "icon": ""
+        },
+        {
+          "id": "site-1756828859792",
+          "name": "净界太公心得（至5.4）",
+          "url": "https://bbs.nga.cn/read.php?tid=26242322",
+          "description": "树歌@拉诺西亚",
+          "icon": ""
+        },
+        {
+          "id": "site-1756828870976",
+          "name": "5.3鱼王图鉴",
+          "url": "https://ngabbs.com/read.php?tid=20293268",
+          "description": "克图格亚w@神意之地",
+          "icon": ""
+        },
+        {
+          "id": "site-1756828880888",
+          "name": "鱼王鱼皇垂钓视频（至5.2）",
+          "url": "https://space.bilibili.com/17759606",
+          "description": "沾到油@静语庄园",
+          "icon": ""
+        }
+      ]
+    },
+    {
+      "id": "category-1756828905086",
+      "icon": "🔍",
+      "name": "其他攻略",
+      "order": 2,
+      "sites": [
+        {
+          "id": "site-1756828934692",
+          "name": "官方攻略书",
+          "url": "https://bbs.nga.cn/read.php?tid=24381144",
+          "description": "",
+          "icon": ""
+        },
+        {
+          "id": "site-1756828942586",
+          "name": "国际服个人测试博客",
+          "url": "https://ffxivfisher.tumblr.com/",
+          "description": "",
+          "icon": ""
+        },
+        {
+          "id": "site-1756828954970",
+          "name": "藏宝图钓场推荐",
+          "url": "https://bbs.nga.cn/read.php?tid=36243403",
+          "description": "轩辕十四@沃仙曦染",
+          "icon": ""
+        },
+        {
+          "id": "site-1756828971112",
+          "name": "FF14理符鱼获取方式推荐",
+          "url": "https://docs.qq.com/sheet/DVFJSZkZ0aXhabkhv",
+          "description": "沾到油@静语庄园",
+          "icon": ""
+        },
+        {
+          "id": "site-1756828985722",
+          "name": "获得力和技能如何影响咬钩时间、脱钩率和提钩数量",
+          "url": "https://bbs.nga.cn/read.php?tid=43421595",
+          "description": "沾到油@静语庄园",
+          "icon": ""
+        },
+        {
+          "id": "site-1756828997486",
+          "name": "鉴别力和技能如何影响鱼的尺寸",
+          "url": "https://bbs.nga.cn/read.php?tid=43230206",
+          "description": "沾到油@静语庄园",
+          "icon": ""
+        },
+        {
+          "id": "site-1756829008640",
+          "name": "捕鱼人部分钓场快速路",
+          "url": "https://bbs.nga.cn/read.php?tid=42537474",
+          "description": "沾到油@静语庄园",
+          "icon": ""
+        },
+        {
+          "id": "site-1756829021850",
+          "name": "捕鱼人100级技能解析",
+          "url": "https://bbs.nga.cn/read.php?tid=40940481",
+          "description": "国际服测试",
+          "icon": ""
+        },
+        {
+          "id": "site-1756829033762",
+          "name": "捕鱼人大地橙票紫票获取方式",
+          "url": "https://bbs.nga.cn/read.php?tid=42046664",
+          "description": "轩辕十四@沃仙曦染",
+          "icon": ""
+        },
+        {
+          "id": "site-1756829044700",
+          "name": "7.0理符鱼钓法",
+          "url": "https://bbs.nga.cn/read.php?tid=41933963",
+          "description": "plas_g",
+          "icon": ""
+        },
+        {
+          "id": "site-1756829056778",
+          "name": "捕鱼人技能后摇和打断6.58版本",
+          "url": "https://bbs.nga.cn/read.php?tid=40852718",
+          "description": "沾到油@静语庄园",
+          "icon": ""
+        },
+        {
+          "id": "site-1756829068750",
+          "name": "捕鱼人计时宏推荐",
+          "url": "https://bbs.nga.cn/read.php?tid=40852159",
+          "description": "沾到油@静语庄园",
+          "icon": ""
+        },
+        {
+          "id": "site-1756829080806",
+          "name": "钓场具体范围可视化",
+          "url": "https://bbs.nga.cn/read.php?tid=24154533",
+          "description": "轩辕十四@沃仙曦染",
+          "icon": ""
+        },
+        {
+          "id": "site-1756829091856",
+          "name": "鱼眼改动与影响一览",
+          "url": "https://ngabbs.com/read.php?tid=26245434",
+          "description": "梦洄子时@红玉海",
+          "icon": ""
+        },
+        {
+          "id": "site-1756829105312",
+          "name": "鱼类的天气和时间条件判定机制",
+          "url": "https://ngabbs.com/read.php?tid=25080307",
+          "description": "久藤彩鸟@沃仙曦染",
+          "icon": ""
+        },
+        {
+          "id": "site-1756829117368",
+          "name": "金属亮片钓鱼研究",
+          "url": "https://bbs.nga.cn/read.php?tid=25284991",
+          "description": "轩辕十四@沃仙曦染",
+          "icon": ""
+        },
+        {
+          "id": "site-1756829128238",
+          "name": "拍水专项研究",
+          "url": "https://bbs.nga.cn/read.php?tid=25250177",
+          "description": "轩辕十四@沃仙曦染",
+          "icon": ""
+        },
+        {
+          "id": "site-1756829139716",
+          "name": "钓场分布图",
+          "url": "https://ngabbs.com/read.php?tid=18140243",
+          "description": "言小叶yxy@红玉海",
+          "icon": ""
+        }
+      ]
     },
     {
       "id": "category-1756828359759",
       "icon": "👥",
       "name": "同人",
-      "order": 2,
+      "order": 3,
       "sites": []
     },
     {
       "id": "category-1756828373545",
       "icon": "🛠️",
       "name": "工具&社群",
-      "order": 3,
+      "order": 4,
       "sites": []
     },
     {
       "id": "category-1756828402993",
       "icon": "📊",
       "name": "成就&紫武",
-      "order": 4,
+      "order": 5,
       "sites": []
     },
     {
       "id": "category-1756828418025",
       "icon": "🌐",
       "name": "海钓/空岛/宇宙",
-      "order": 5,
+      "order": 6,
       "sites": []
     }
   ],
